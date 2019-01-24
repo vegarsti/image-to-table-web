6d299c6498d468946e41b3ede135bb29c8a59082
Vegard Stikbakke <vegard.stikbakke@gmail.com>
2019-01-24 10:58:54 +0100

    Correctly extracts tables from several examples

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import sys
 import pytesseract
 from collections import namedtuple
 import time
+import statistics
+import itertools
+import csv
 
 try:
     from PIL import Image
@@ -15,6 +18,8 @@ if len(sys.argv) != 3:
 
 filename = sys.argv[1]
 image = cv2.imread(filename)
+height, width, _ = image.shape  # assumes color image
+picture_size = height * width
 
 number_of_columns = int(sys.argv[2])
 if number_of_columns < 1:
@@ -26,64 +31,39 @@ if number_of_columns < 1:
 fields_string = "level left top width height conf text"
 fields = fields_string.split()
 
-tesseract_config = "--psm 6"  # assume a single uniform block of text
+tesseract_config = "--psm 6 -l nor"  # assume a single uniform block of text
 data = pytesseract.image_to_data(
     image, config=tesseract_config, output_type=pytesseract.Output.DICT
 )
 for field, values in data.items():
-    # print(field)
     N = len(values)
 
-characters = [{} for i in range(N)]
+boxes = [{} for i in range(N)]
 for field, values in data.items():
     if field in fields:
         for i, value in enumerate(values):
-            characters[i][field] = value
+            boxes[i][field] = value
 
-for character in characters:
-    character["right"] = character["left"] + character["width"]
-    character["bottom"] = character["top"] + character["height"]
-    character["size"] = character["width"] * character["height"]
+for box in boxes:
+    box["right"] = box["left"] + box["width"]
+    box["bottom"] = box["top"] + box["height"]
+    box["size"] = box["width"] * box["height"]
 
-Character = namedtuple("Character", sorted(characters[0]))
-characters = [Character(**character) for character in characters]
-# print(characters[0])
+Box = namedtuple("Box", sorted(boxes[0]))
+boxes = [Box(**box) for box in boxes]
+# print(boxes[0])
 
 lefts = []
 rights = []
 tops = []
 bottoms = []
 
-for character in characters:
-    # print(character.left, character.right)
-    lefts.append(character.left)
-    rights.append(character.right)
-    tops.append(character.top)
-    bottoms.append(character.bottom)
-
-# print(sorted(lefts))
-# print(sorted(rights))
-# print()
-# print(sorted(tops))
-# print(sorted(bottoms))
-
-left_diffs = []
-for i, _ in enumerate(lefts):
-    if i == 0:
-        continue
-    left_diffs.append(lefts[i] - lefts[i - 1])
-
-right_diffs = []
-for i, _ in enumerate(rights):
-    if i == 0:
-        continue
-    right_diffs.append(rights[i] - rights[i - 1])
-
-"""
-print()
-print(left_diffs)
-print(right_diffs)
-"""
+for box in boxes:
+    # print(box.left, box.right)
+    lefts.append(box.left)
+    rights.append(box.right)
+    tops.append(box.top)
+    bottoms.append(box.bottom)
 
 
 def boxes_equal(b1, b2):
@@ -92,8 +72,8 @@ def boxes_equal(b1, b2):
     return equal_size and equal_top
 
 
-def is_character_to_right_of_line(character, x):
-    return character.left > x
+def is_box_to_right_of_line(box, x):
+    return box.left > x
 
 
 def is_box_inside_other_box(box1, box2):
@@ -107,64 +87,12 @@ def is_box_inside_other_box(box1, box2):
     return size and left and right and top and bottom
 
 
-left_column = []
-right_column = []
-
-tentative_dividing_x = 400
-
-for character in characters:
-    if is_character_to_right_of_line(character, tentative_dividing_x):
-        right_column.append(character)
-    else:
-        left_column.append(character)
-
-# print(len(left_column))
-# print(len(right_column))
-
-for r in right_column:
-    pass
-    # print(r.text)
-
-for l in left_column:
-    pass
-    # print(l.text)
-
-for i, character in enumerate(characters):
-    pass
-    # print(character.text, i)
-
-
-def character_distance(c1, c2):
-    x = min(abs(c2.left - c1.right), abs(c1.left - c2.right))
-    y = min(abs(c2.top - c1.bottom), abs(c1.top - c2.bottom))
-    return x ** 2 + y ** 2
-
-
-def find_nearest_character(character, characters):
-    # assume non-overlapping
-    return min(characters, key=lambda c: character_distance(character, c))
-
-
-def calculate_box_size(character):
-    return character.width * character.height
-
-
-# print()
-
-i = 4
-character = characters[i]
-near = find_nearest_character(character, characters)
-# print(character.text)
-# print(near.text)
-
 """
 cv2.imshow("Some title", image)
 cv2.waitKey(0)
 """
 
-height, width, _ = image.shape  # assumes color image
-
-sorted_boxes = sorted(characters, key=lambda c: c.size, reverse=True)
+sorted_boxes = sorted(boxes, key=lambda c: c.size, reverse=True)
 
 number_of_lines = 0
 
@@ -176,22 +104,13 @@ show_all_boxes = False
 
 for box_num, box in enumerate(sorted_boxes):
     count = True
-    if box_num == 0:  # entire image
-        count = False
     previous_box = sorted_boxes[box_num - 1]
-    if box_num == 1:  # table
-        count = False
-    # print("height: ", box.height)
-    # print("width: ", box.width)
-    # print("top: ", box.top)
     if boxes_equal(box, previous_box):
         count = False
+    if box.size > 0.3 * picture_size:  # some threshold
+        count = False
     if box.text.strip() == "" and count:
-        # print(box_num)
         number_of_lines += 1
-        # print(box.left, box.top)
-        # print(box.right, box.bottom)
-        # print()
         lines.append(box)
     if show_all_boxes:
         image = cv2.rectangle(
@@ -199,22 +118,6 @@ for box_num, box in enumerate(sorted_boxes):
         )
         cv2.imshow("title", image)
         cv2.waitKey(0)
-
-    """
-    if box_num > 3 and is_box_inside_other_box(box, line_box):
-        image = cv2.rectangle(
-            image, (box.left, box.top), (box.right, box.bottom), (0, 255, 0), 2
-        )
-        cv2.imshow("title", image)
-        cv2.waitKey(0)
-    """
-
-# print(number_of_lines)
-
-# print()
-# print()
-
-line_dicts = []
 
 
 def find_index_of_n_largest(items, n):
@@ -229,6 +132,8 @@ def find_index_of_n_largest(items, n):
     return sorted([i + 1 for i in indexes])
 
 
+all_divisions = []
+line_dicts = []
 for i, line_box in enumerate(lines):
     # find those boxes which are inside this line
     line_dict = {}
@@ -246,13 +151,13 @@ for i, line_box in enumerate(lines):
         for i in range(1, len(line_dict["word_boxes"]))
     ]
 
-    if line_dict["words"][0] == "John":
-        print(line_dict["word_boxes"])
-
     indexes = find_index_of_n_largest(diffs, number_of_columns)
-    # print(indexes)
-    # time.sleep(5)
     group_words = []
+    divisions = [
+        (line_dict["word_boxes"][index].left, line_dict["word_boxes"][index - 1].right)
+        for index in indexes
+    ]
+    all_divisions.append(divisions)
     for i, index in enumerate(indexes):
         if i == 0:
             start = 0
@@ -272,5 +177,55 @@ for i, line_box in enumerate(lines):
     line_dicts.append(line_dict)
 
 sorted_lines = sorted(line_dicts, key=lambda l: l["bounding_box"].top)
-for line in sorted_lines:
-    print(",".join(line["group_words"]))
+
+all_midpoints = []
+all_lefts = []
+for divisions in all_divisions:
+    midpoints = []
+    lefts = []
+    for left, right in divisions:
+        midpoints.append(right + (left - right) / 2)
+        lefts.append(left)
+    all_midpoints.append(midpoints)
+    all_lefts.append(lefts)
+
+transposed_midpoints = list(zip(*all_midpoints))
+transposed_lefts = list(zip(*all_lefts))
+
+dividing_points = []
+for column_midpoints in transposed_midpoints:
+    dividing_points.append(int(max(column_midpoints)))
+
+
+all_lists = []
+
+
+def partition(items, predicate=bool):
+    a, b = itertools.tee((predicate(item), item) for item in items)
+    return ((item for pred, item in a if not pred), (item for pred, item in b if pred))
+
+
+rows_strings = []
+rows = []
+for line_dict in sorted_lines:
+    points_to_left = line_dict["word_boxes"]
+    cells = []
+    for i, dividing_point in enumerate(dividing_points):
+        points_to_left, points_to_right = partition(
+            points_to_left, lambda word_box: word_box.right > dividing_point
+        )
+        points_to_left = list(points_to_left)
+        points_to_right = list(points_to_right)
+        text = " ".join(p.text for p in points_to_left)
+        text = text.replace(",", ".")
+        cells.append(text)
+        points_to_left = points_to_right
+    cells.append(" ".join(p.text for p in points_to_left))
+    rows_strings.append(",".join(cells))
+    rows.append(cells)
+
+filename_csv = filename.split(".")[0] + ".csv"
+with open(filename_csv, "w") as csv_file:
+    wr = csv.writer(csv_file, delimiter=",")
+    for row in rows:
+        wr.writerow(row)
