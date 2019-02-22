cbcb63c7fc58fa89697be73e7978d016d0b014c1
Vegard Stikbakke <vegard.stikbakke@gmail.com>
2019-02-22 12:02:05 +0100

    Remove explore

--- app/routes.py
+++ app/routes.py
@@ -49,24 +49,6 @@ def index():
     )
 
 
-@app.route("/explore")
-@login_required
-def explore():
-    page = request.args.get("page", 1, type=int)
-    posts = Post.query.order_by(Post.timestamp.desc()).paginate(
-        page, app.config["POSTS_PER_PAGE"], False
-    )
-    next_url = url_for("explore", page=posts.next_num) if posts.has_next else None
-    prev_url = url_for("explore", page=posts.prev_num) if posts.has_prev else None
-    return render_template(
-        "index.html",
-        title="Explore",
-        posts=posts.items,
-        next_url=next_url,
-        prev_url=prev_url,
-    )
-
-
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if current_user.is_authenticated:
