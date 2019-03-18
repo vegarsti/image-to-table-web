9ae7ad17786c390eb8666d4dc7c6afc33051ac35
Vegard Stikbakke <vegard.stikbakke@gmail.com>
2019-03-18 13:37:14 +0100

    AWS related things

---
+++ aws_lambda_things.py
@@ -0,0 +1,31 @@
+import boto3
+import os
+import json
+from dotenv import load_dotenv
+
+load_dotenv()
+
+lambda_client = boto3.client("lambda")
+AWS_ID = os.getenv("AWS_ID")
+
+function_name = "HelloWorld"
+function_role = f"arn:aws:iam::{AWS_ID}:role/lambda_basic_execution"
+
+lambda_client.create_function(
+    FunctionName=function_name,
+    Runtime="python3.7",
+    Role=function_role,
+    Handler=f"{function_name}.lambda_handler",
+    Code={"ZipFile": open(f"{function_name}.zip", "rb").read()},
+)
+
+
+def lambda_funcs():
+    client = boto3.client("lambda")
+    response = client.list_functions()
+    lambda_name = response["Functions"][0]["FunctionName"]
+    event = {"first_name": "Vegard", "last_name": "Stikbakke"}
+    event_json = json.dumps(event)
+    resp = client.invoke(FunctionName=lambda_name, Payload=event_json)
+    function_response = resp["Payload"].read()
+    print(function_response)
