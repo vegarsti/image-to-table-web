4790751bc00122d3636e83e9ebf13975203be354
Vegard Stikbakke <vegard.stikbakke@gmail.com>
2019-03-21 14:33:36 +0100

    Add the wsgi config file

--- wsgi.py
+++ wsgi.py
@@ -5,3 +5,7 @@ from app.models import User, Image
 @app.shell_context_processor
 def make_shell_context():
     return {"db": db, "User": User, "Image": Image}
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
